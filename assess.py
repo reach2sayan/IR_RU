d200be81f20294f2f81f512d7bbfd671d1f25ec3
Sayan Samanta <sayan_samanta@brown.edu>
2019-07-17 20:58:27 -0400

    extraction of input data for assess script

--- assess.py
+++ assess.py
@@ -1,14 +1,16 @@
 import numpy as np
 import os
 
-elems = []
+elems = [] #holds all elements
 sites = []
-spec = {}
-phases = []
-allPhase = []
-numOfparams = 0
+spec = {} #holds species hashed by site 
+phases = [] #holds all phases calculated
+terms = {}
+allPhase = {} #holds all sites for all phase
+numOfparams = 0 #records no. of params to fit with OC
 valueList = ('energy','svib_ht')
 phaseList = os.listdir("/users/ssamanta/atat/data/sqsdb/")
+
 def readInput():
     sf = open("species.in","r")
     
@@ -18,17 +20,40 @@ def readInput():
     tempPhases = [file for file in files if file in phaseList]
 
     for phase in tempPhases:
-        fileSpec = "./{}/species.in".format(phase)
-        pf = open(fileSpec,"r")
+        sites = []
+        spec = {}
+        fileSpecPhase = "./{}/species.in".format(phase)
+        pf = open(fileSpecPhase,"r")
         rawsite = (pf.read().strip("\n")).split(" ")
         for s in range(0,len(rawsite)):
             asitespec = rawsite[s].split("=")
+            site = asitespec[0]
+            sites.append(site)
+            spec.update({site: asitespec[1].split(",")})
             
-    #count how many parameters to be fit
-    #for end-members G terms
-    for phase in phases:
-        for elem in elems:
-            numOfparams = numberOfparams + 2
-    #for L terms
+        allPhase.update({phase:spec})
+        pf.close()
+        
+        atermraw = []
+        termsTemp = []
+        termPerPhase = []
+        fileTerm = "./{}/terms.in".format(phase)
+        tf = open(fileTerm,"r")
+        for line in tf:
+            line = line.strip("\n")
+            atermraw = line.split(":")
+            for item in atermraw:
+                aterm = item.split(",")
+                termsTemp.append(aterm)
+            termPerPhase.append(termsTemp)
+            termsTemp = []
+        terms.update({phase:termPerPhase})
 
-    
+        numOfparams = 0
+    for phase in terms:
+        for term in terms[phase]:
+            for siteTerm in term:
+                numOfparams = numOfparams + (len(elems)//int(siteTerm[0]))*(int(siteTerm[1])+1)
+    return [elems, allPhase, terms, numOfparams]
+
+[elems, allPhase, terms, numOfparams] = readInput()
